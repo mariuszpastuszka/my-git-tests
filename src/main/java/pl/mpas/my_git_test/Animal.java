e63e7cad70f1aee1521a4c3b0a822803f57a4ec0
maniek <mpastuszka@gmail.com>
2018-12-01 13:08:03 +0100

    [change] - adding getters and setters to Animal class

--- src/main/java/pl/mpas/my_git_test/Animal.java
+++ src/main/java/pl/mpas/my_git_test/Animal.java
@@ -7,4 +7,12 @@ public class Animal {
     public Animal(String name) {
         this.name = name;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
