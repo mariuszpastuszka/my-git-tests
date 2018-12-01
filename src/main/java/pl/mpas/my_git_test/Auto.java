65597b760fb7c7b597997503f09379403980cabd
maniek <mpastuszka@gmail.com>
2018-12-01 11:49:23 +0100

    [change] - zmiana nazwy klasy z Car'a na Auto

--- src/main/java/pl/mpas/my_git_test/Auto.java
+++ src/main/java/pl/mpas/my_git_test/Auto.java
@@ -1,18 +1,18 @@
 package pl.mpas.my_git_test;
 
-public class Car {
+public class Auto {
 
     private String brand;
     private String model;
 
-    public Car(String brand, String model) {
+    public Auto(String brand, String model) {
         this.brand = brand;
         this.model = model;
     }
 
     @Override
     public String toString() {
-        return "Car{" +
+        return "Auto{" +
                 "brand='" + brand + '\'' +
                 ", model='" + model + '\'' +
                 '}';
